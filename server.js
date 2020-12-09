fc35af2141005e5ebedb4859b1e0dbf9eacf185c
Mathew B <bogaert@email.arizona.edu>
2020-12-08 17:43:50 -0700

    findById

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { animals } = require('./data/animals');
-const express = require('express');  
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 // instantiate the server (create the server)
@@ -39,6 +39,11 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+};
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -47,7 +52,16 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
-})
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 // makes the server listen
 app.listen(PORT, () => {
